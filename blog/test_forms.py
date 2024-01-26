5c549b0aaa0be937984786345cff7b9edc833ebe
Gkicks <>
2024-01-26 17:27:45 +0000

    formatting changes following validation

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .forms import RecipeForm, CommentForm, RatingForm
 from .models import Recipe
 
+
 class TestRecipeForm(TestCase):
     """
     test to check if the recipe form is valid
@@ -10,36 +11,46 @@ class TestRecipeForm(TestCase):
         """
         correct details entered
         """
-        recipe_form = RecipeForm ({
+        recipe_form = RecipeForm({
             'recipe_name': 'cherry Pie',
             'category': '2',
             'ingredients': '1 spoonful of sugar',
             'method': 'bake in a pie',
             'servings': '4'
         })
-        self.assertTrue(recipe_form.is_valid(), msg="recipe form is not valid")
+        self.assertTrue(
+            recipe_form.is_valid(), msg="recipe form is not valid")
 
     # recipe_name field
     def test_recipe_name_empty(self):
         """
         empty recipe_name
         """
-        recipe_form = RecipeForm ({'recipe_name': ''})
-        self.assertFalse(recipe_form.is_valid(), msg="recipe form valid with empty recipe_name")
+        recipe_form = RecipeForm({'recipe_name': ''})
+        self.assertFalse(
+            recipe_form.is_valid(), msg="form valid with empty recipe_name")
 
     def test_recipe_name_too_long(self):
         """
         recipe_name more than 255 characters
         """
-        recipe_form = RecipeForm ({'recipe_name': 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'})
-        self.assertFalse(recipe_form.is_valid(), msg="recipe form valid with 260 characters")
+        recipe_form = RecipeForm(
+            {'recipe_name': 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw'
+                'xyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdef'
+                'ghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmno'
+                'pqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwx'
+                'yzabcdefghijklmnopqrstuvwxyz'}
+                            )
+        self.assertFalse(
+            recipe_form.is_valid(), msg="form valid with 260 characters")
 
     def test_recipe_name_not_unique(self):
         """
         none unique recipe_name
         """
-        recipe_form = RecipeForm ({'recipe_name': 'Tofu Fillets'})
-        self.assertFalse(recipe_form.is_valid(), msg="recipe form valid with duplicate recipe name")
+        recipe_form = RecipeForm({'recipe_name': 'Tofu Fillets'})
+        self.assertFalse(
+            recipe_form.is_valid(), msg="form valid with non-unique name")
 
 
 class TestCommentForm(TestCase):
@@ -51,14 +62,16 @@ class TestCommentForm(TestCase):
         string entered
         """
         comment_form = CommentForm({'body': 'I love this recipe'})
-        self.assertTrue(comment_form.is_valid(), msg="comment form is not valid")
+        self.assertTrue(
+            comment_form.is_valid(), msg="comment form is not valid")
 
     def test_form_is_not_valid(self):
         """
         empty string entered
         """
         comment_form = CommentForm({'body': ''})
-        self.assertFalse(comment_form.is_valid(), msg="comment form is valid with empty string")
+        self.assertFalse(
+            comment_form.is_valid(), msg="form is valid with empty string")
 
 
 class TestRatingForm(TestCase):
@@ -77,32 +90,38 @@ class TestRatingForm(TestCase):
         empty string entered
         """
         rating_form = RatingForm({'rating': ''})
-        self.assertFalse(rating_form.is_valid(), msg="rating form is valid with empty string")
+        self.assertFalse(
+            rating_form.is_valid(), msg="form is valid with empty string")
 
     def test_form_is_not_valid_other_int(self):
         """
         integer outside range 1-5 entered
         """
         rating_form = RatingForm({'rating': '6'})
-        self.assertFalse(rating_form.is_valid(), msg="rating form is valid with integer not in range 1-5")
-    
+        self.assertFalse(
+            rating_form.is_valid(),
+            msg="form is valid with int not in range 1-5")
+
     def test_form_is_not_valid_negative(self):
         """
         negative number entered
         """
         rating_form = RatingForm({'rating': '-5'})
-        self.assertFalse(rating_form.is_valid(), msg="rating form is valid with negative number")
+        self.assertFalse(
+            rating_form.is_valid(), msg="form is valid with negative number")
 
     def test_form_is_not_valid_letter(self):
         """
         letter entered
         """
         rating_form = RatingForm({'rating': 'j'})
-        self.assertFalse(rating_form.is_valid(), msg="rating form is valid with letter")
+        self.assertFalse(
+            rating_form.is_valid(), msg="rating form is valid with letter")
 
     def test_form_is_not_valid_special(self):
         """
         special character entered
         """
         rating_form = RatingForm({'rating': '%'})
-        self.assertFalse(rating_form.is_valid(), msg="rating form is valid with special character")
+        self.assertFalse(
+            rating_form.is_valid(), msg="form is valid with special character")
